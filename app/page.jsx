7c2ecb304fa04c768bfee2ee591c6374276551ac
Kaori Kawakami <k.kawakami08@gmail.com>
2024-12-11 11:18:13 -0800

    Navbar and infoboxes on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,8 @@
+import properties from "@/properties.json";
+
+const PropertiesPage = () => {
+  console.log(properties);
+  return <div>PropertiesPage</div>;
+};
+
+export default PropertiesPage;
